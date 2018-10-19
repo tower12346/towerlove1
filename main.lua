1b3689fac7e5cd0fac052147d66b62d75384fd1b
tower12346 <tower12346@gmail.com>
2018-10-19 17:24:57 +0800

    Perfected gravity, simplified platform additions

--- main.lua
+++ main.lua
@@ -1,54 +1,77 @@
+function makePlatform(x1, y1, w1, h1, iF)
+	local platform = {
+		x = x1,
+		y = y1,
+		w = w1,
+		h = h1,
+		isFloor = iF
+	}
+	return platform
+end
+
 function love.load()
 	bump = require 'bump'
 	world = bump.newWorld()
 
 	player = {
-		x = 0,
+		x = 1,
 		y = 500,
 		yv = 0,
 		yt = 0,
 		w = 10,
 		h = 10
 	}
-	platform1 = {
-		x = 50,
-		y = 450,
-		w = 30,
-		h = 30
+
+	platforms = {
+		makePlatform(50,450,30,30,true), 
+		makePlatform(100,400,30,30,true), 
+		makePlatform(150,350,30,30,false)
 	}
+
 	floor = {
 		x = 1,
-		y = 500,
-		w = 500,
-		h = 1
+		y = 510,
+		w = 510,
+		h = 10,
+		isFloor = true
 	}
 	world:add(player, player.x, player.y, player.w, player.h)
-	world:add(platform1, platform1.x, platform1.y,  platform1.w, platform1.h)
-	--world:add(floor, floor.x, floor.y, floor.w, floor.h)
+	for i,v in ipairs (platforms) do
+		world:add(platforms[i], platforms[i].x, platforms[i].y, platforms[i].w, platforms[i].h)
+	end
+	world:add(floor, floor.x, floor.y, floor.w, floor.h)
+end
+
+local playerFilter = function(item, other)
+  --if     other.isCoin   then return 'cross'
+  --elseif other.isWall   then return 'slide'
+  --elseif other.isExit   then return 'touch'
+  --elseif other.isSpring then return 'bounce'
+  --end
+  -- else return nil
+  return 'slide'
 end
 
 function love.update(dt)
 	local goalX = player.x 
 	local goalY = player.y
 	if love.keyboard.isDown("right") then
-		goalX = player.x + 60 * dt
+		goalX = player.x + 100 * dt
 	elseif love.keyboard.isDown("left") then
-		goalX = player.x - 60 * dt
+		goalX = player.x - 100 * dt
 	end
 
 	if love.keyboard.isDown("up") then
-		player.yv = 1
+		player.yv = 2
 	end
-	if player.yv > 0 then
-		player.yt = player.yt + 1
-	end
-	goalY = player.y - player.yv*player.yt + 0.5*0.1*player.yt^2
+	goalY = player.y - player.yv*player.yt + 0.1*player.yt^2
 	local actualX, actualY, cols, len = world:move(player, goalX, goalY)
-
-	if actualY>=500 then
-		player.yt = 0
-		player.yv = 0
-		actualY = 500
+	player.yt = player.yt + 1
+	for i,v in ipairs (cols) do	
+		if cols[i].other.isFloor and cols[i].normal.y == -1 then
+	    	player.yt = 0
+			player.yv = 0
+		end
 	end
 
 	player.x, player.y = actualX, actualY
@@ -57,5 +80,7 @@ end
 function love.draw()
 	love.graphics.print(math.floor(player.x).." " .. math.floor(player.y) .." ".. math.floor(player.yt) .. " " .. math.floor(player.yv), 400, 300)
     love.graphics.rectangle("fill", player.x, player.y, player.w, player.h)
-    love.graphics.rectangle("fill", platform1.x, platform1.y, platform1.w, platform1.h)
+    for i, v in ipairs (platforms) do
+    	love.graphics.rectangle("fill", platforms[i].x, platforms[i].y, platforms[i].w, platforms[i].h)
+    end
 end
